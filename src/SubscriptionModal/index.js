5ffd21238caee45822cbc91d9b2c8ac13c08a652
Mercury Minds <jagan.ganesh@mercuryminds.com>
2020-02-03 18:02:20 +0530

    Change test payment to production

--- src/SubscriptionModal/index.js
+++ src/SubscriptionModal/index.js
@@ -383,22 +383,22 @@ class SubscriptionModalNavigation extends React.Component {
         // processing payment
 
         // for live account payU
-       // newOrder.Create({
-           // amount: this.state.planSelected.value,
-          //  productinfo: this.state.params.info.currentLevelName,
-          //  firstname: params.name,
-          //  email: params.email,
-          //  phone: params.phone,
-          //  surl: 'https://www.google.com/_success',
-         //   furl: 'https://www.google.com/_failure',
-         //   service_provider: 'payuBiz',
-          //  txnid: uuid.v4(),
-          //  key: this.props.user.userIN ? "7dr1rA" : "fDBTdB",
-          //  salt: this.props.user.userIN ? "vLEDVf0x" : "FKU2QUeq",
-        //}, true);
+       newOrder.Create({
+           amount: this.state.planSelected.value,
+           productinfo: this.state.params.info.currentLevelName,
+           firstname: params.name,
+           email: params.email,
+           phone: params.phone,
+           surl: 'https://www.google.com/_success',
+           furl: 'https://www.google.com/_failure',
+           service_provider: 'payuBiz',
+           txnid: uuid.v4(),
+           key: this.props.user.userIN ? "7dr1rA" : "fDBTdB",
+           salt: this.props.user.userIN ? "vLEDVf0x" : "FKU2QUeq",
+        }, true);
 
         //for test account payU
-         newOrder.Create({
+         /* newOrder.Create({
             amount: this.state.planSelected.value,
              productinfo: this.state.params.info.currentLevelName,
              firstname: params.name,
@@ -408,7 +408,7 @@ class SubscriptionModalNavigation extends React.Component {
              furl: 'https://www.google.com/_failure',
              service_provider: 'payuBiz',
              txnid: uuid.v4(),
-         }, false);
+         }, false); */
 
         newOrder.sendReq()
             .then(Response => {
